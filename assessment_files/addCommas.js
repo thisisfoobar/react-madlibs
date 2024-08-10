6aee35aed9cfcbd43ea51969efd881d84b1e8676
thisisfoobar <matt.mcelreath26@gmail.com>
2024-08-10 17:56:47 -0400

    Add commas function and tests

--- assessment_files/addCommas.js
+++ assessment_files/addCommas.js
@@ -1,3 +1,19 @@
-function addCommas() {}
+function addCommas(num) {
+  let stringNum = num.toString()
+  let result = "";
+  let count = 0;
+
+  if (stringNum.length <= 3) return stringNum
+  for (let i = stringNum.length - 1; i >= 0; i --) {
+    result = stringNum[i] + result
+    count++;
+
+    if (count % 3 === 0 && i !== 0) {
+      result = "," + result;
+    }
+  }
+
+  return result;
+}
 
 module.exports = addCommas;
--- assessment_files/addCommas.test.js
+++ assessment_files/addCommas.test.js
@@ -4,4 +4,21 @@ describe("#addCommas", () => {
   test("it is a function", () => {
     expect(typeof addCommas).toBe("function");
   });
+
+  test("it returns a string value", () => {
+    const res = addCommas(123)
+    expect(typeof res).toBe("string")
+  })
+
+  test("it does NOT add commas", () => {
+    const res = addCommas(123)
+    expect(res).not.toContain(",")
+  })
+
+  test("it adds commas to number", () => {
+    const res = addCommas(12345)
+    expect(res).toContain(",")
+    expect(typeof res).toBe("string")
+    expect(res).toEqual("12,345")
+  })
 });
